c501fd7db2ffa2116ed41dd60831d80e8e766cc2
Lian <laix678@gmail.com>
2024-08-18 19:01:02 +0800

    修正_前端顯示和後端處理

--- python/app.py
+++ python/app.py
@@ -13,7 +13,9 @@ def index():
 def send_message():
     data = request.json
     print("Received data:", data)  # 打印接收到的所有數據，用於調試
+    # 輸出 Received data: {'groupCount': 3, 'group1': {'title': '前言a', 'content': 'a'}, 'group2': {'title': '關於公司', 'content': 'a'}, 'group3': {'title': '公司治理', 'content': 'a'}}
 
+    # 如果 groupCount 不存在，則預設為 0
     group_count = data.get('groupCount', 0)
     groups_data = []
 
@@ -22,15 +24,31 @@ def send_message():
         if group_key in data:
             groups_data.append(data[group_key])
 
+    # 輸出 處理數據：[{'title': '前言a', 'content': 'a'}, {'title': '關於公司', 'content': 'a'}, {'title': '公司治理', 'content': 'a'}]
+    print(f"處理數據：{groups_data}")
+
     # 這裡你可以處理接收到的數據
     # 例如，你可以將數據傳遞給一個函數來生成 ESG 報告
-    # response = generate_esg_report(groups_data)
+    generate_esg_report(groups_data)
 
     # 暫時的示例響應
     response = f"Received {group_count} groups of data. Processing..."
+    response = f"{groups_data}"
 
     print(f"Output: {response}")
+    
+    # 
     return jsonify({"response": response})
 
+# 生成 ESG 報告的函數
+def generate_esg_report(groups_data):
+    print("處理中...")
+    # groups_data = [{'title': '前言a', 'content': 'a'}, {'title': '關於公司', 'content': 'a'}, {'title': '公司治理', 'content': 'a'}]
+    # 將 每一個 content 加入 "hello" 字串
+    for group in groups_data:
+        group['content'] = f"hello {group['content']}"
+    # print(f"ESG 報告生成成功！")
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # 啟動 Flask 服務
